84687bf5dd3de328611ce7b6b0d28c78d8fcc04e
bullet3113 <0hitesh.a@gmail.com>
2023-12-27 21:01:26 +0530

    initial commit

---
+++ src/test/java/com/stockTrading/StockTradingApplication/StockTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockTrading.StockTradingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
